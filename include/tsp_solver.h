6a4057a9751b627c784b70fb627a1f3899f0379a
AperMesa <emeric.courtade@orange.fr>
2025-08-28 11:44:59 +0200

    add point struct and euclidean distance

---
+++ include/tsp_solver.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Point {
+    double x;
+    double y;
+};
+
+double euclidean_distance(const Point& a, const Point& b);
---
+++ src/tsp_solver.cpp
@@ -0,0 +1,8 @@
+#include "tsp_solver.h"
+#include <cmath>
+
+double euclidean_distance(const Point& a, const Point& b) {
+    double dx = a.x - b.x;
+    double dy = a.y - b.y;
+    return std::sqrt(dx*dx + dy*dy);
+}
