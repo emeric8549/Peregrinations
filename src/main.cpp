a7fb724f078c62fee95a3f97ffeb5762a5c7788c
Emeric <emeric.courtade@orange.fr>
2025-09-22 09:46:25 +0000

    add 2-opt algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "distances.h"
 #include "data/data_generator.h"
 #include "nearest_neighbor.h"
+#include "two_opt.h"
 
 int main(int argc, char* argv[]) {
     int N = 10;
@@ -13,6 +14,7 @@ int main(int argc, char* argv[]) {
     std::function<double(const Point&, const Point&)> metric_func = euclidean_distance;
     bool calculate_simple_tour = false;
     bool calculate_nn_tour = false;
+    bool calculate_2opt_tour = false;
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
@@ -36,6 +38,8 @@ int main(int argc, char* argv[]) {
             calculate_simple_tour = true;
         } else if (arg == "--calculate-nn-tour") {
             calculate_nn_tour = true;
+        } else if (arg == "--calculate-2opt-tour") {
+            calculate_2opt_tour = true;
         } else {
             std::cerr << "Unknown argument: " << arg << std::endl;
             return 1;
@@ -75,6 +79,20 @@ int main(int argc, char* argv[]) {
             const auto end{std::chrono::steady_clock::now()};
             std::cout << "Length of nearest neighbor tour: " << tour_length << std::endl;
             std::cout << "Time taken for nearest neighbor tour: "
+                      << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+                      << " microseconds" << std::endl;
+        }
+    }
+
+    if (calculate_2opt_tour) {
+        if (N < 2) {
+            std::cout << "Not enough points to calculate a tour." << std::endl;
+        } else {
+            const auto start{std::chrono::steady_clock::now()};
+            auto [tour, tour_length] = two_opt_tour(points, metric_func);
+            const auto end{std::chrono::steady_clock::now()};
+            std::cout << "Length of 2-opt tour: " << tour_length << std::endl;
+            std::cout << "Time taken for 2-opt tour: "
                       << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
                       << " microseconds" << std::endl;
         }
