4e2fb6b3532052a3934cb381a78cd9a8c6e2df80
Emeric <emeric.courtade@orange.fr>
2025-09-22 09:43:28 +0000

    implement 2-opt algorithm

--- src/two_opt.cpp
+++ src/two_opt.cpp
@@ -1,6 +1,8 @@
 #include "two_opt.h"
+#include <numeric>
+#include <algorithm>
 
-std::pair<std::vector<int>, double> two_opt_tour(const std::vector<Point>& points, const std::function<double(const Point&)> metric_func) {
+std::pair<std::vector<int>, double> two_opt_tour(const std::vector<Point>& points, std::function<double(const Point&, const Point&)> metric_func) {
     int N = points.size();
     if (N < 2) {
         return {{}, 0.0};
@@ -14,10 +16,47 @@ std::pair<std::vector<int>, double> two_opt_tour(const std::vector<Point>& point
 
     std::vector<std::vector<double>> dist_matrix(N, std::vector<double>(N, 0.0));
     for (int i = 0; i < N; ++i) {
-        for (int j = i+1; j < N; ++j) {
+        for (int j = i + 1; j < N; ++j) {
             double dist = metric_func(points[i], points[j]);
             dist_matrix[i][j] = dist;
             dist_matrix[j][i] = dist;
         }
     }
+
+    auto tour_length = [&](const std::vector<int>& t) {
+        double s = 0.0;
+        for (int i = 0; i < N; ++i) {
+            int a = t[i];
+            int b = t[(i + 1) % N];
+            s += dist_matrix[a][b];
+        }
+        return s;
+    };
+
+    double current_length = tour_length(tour);
+
+    bool improved = true;
+    const double epsilon = 1e-12;
+    while (improved) {
+        improved = false;
+        for (int i = 0; i < N - 1 && !improved; ++i) {
+            for (int k = i + 2; k < N + 1; ++k) {
+                int A = tour[i];
+                int B = tour[(i + 1) % N];
+                int C = tour[k];
+                int D = tour[(k + 1) % N];
+
+                double delta = dist_matrix[A][C] + dist_matrix[B][D] - dist_matrix[A][B] - dist_matrix[C][D];
+
+                if (delta < -epsilon) {
+                    std::reverse(tour.begin() + (i + 1), tour.begin() + (k + 1));
+                    current_length += delta;
+                    improved = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    return {tour, current_length};
 }
